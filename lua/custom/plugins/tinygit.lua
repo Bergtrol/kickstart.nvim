0f9a6c067b845138ab70262fd6ec1809c4122190
Bergtrol <jannie.pieters@gmail.com>
2024-04-22 22:53:56 +0200

    Fiddling with tinygit

--- lua/custom/plugins/tinygit.lua
+++ lua/custom/plugins/tinygit.lua
@@ -2,25 +2,25 @@
 -- https://github.com/windwp/nvim-autopairs
 
 return {
-  'chrisgrieser/nvim-tinygit',
-  ft = { 'git_rebase', 'gitcommit' }, -- so ftplugins are loaded
-  dependencies = {
-    'stevearc/dressing.nvim',
-    'nvim-telescope/telescope.nvim', -- either telescope or fzf-lua
-    'ibhagwan/fzf-lua',
-    'rcarriga/nvim-notify', -- optional, but will lack some features without it
+  {
+    'chrisgrieser/nvim-tinygit',
+    ft = { 'git_rebase', 'gitcommit' }, -- so ftplugins are loaded
+    dependencies = {
+      'stevearc/dressing.nvim',
+      'nvim-telescope/telescope.nvim', -- either telescope or fzf-lua
+      'ibhagwan/fzf-lua',
+      'rcarriga/nvim-notify', -- optional, but will lack some features without it
+    },
+    opts = {
+      on_attach = function()
+        require('tinygit').smartCommit { pushIfClean = false } -- options default to `false`
+        vim.keymap.set('n', '<leader>hc', function()
+          require('tinygit').smartCommit()
+        end, { desc = 'Tinygit - SmartCommit' })
+        vim.keymap.set('n', '<leader>hp', function()
+          require('tinygit').push {}
+        end, { desc = 'Tinygit - Push' })
+      end,
+    },
   },
-  opts = {
-    on_attach = function()
-      require('tinygit').smartCommit { pushIfClean = false } -- options default to `false`
-      require('nvim-tinygit').setup {}
-      vim.keymap.set('n', '<leader>hc', function()
-        require('tinygit').smartCommit()
-      end, { desc = 'Tinygit - SmartCommit' })
-      vim.keymap.set('n', '<leader>hp', function()
-        require('tinygit').push {}
-      end, { desc = 'Tinygit - Push' })
-    end,
-  },
-  config = function() end,
 }
