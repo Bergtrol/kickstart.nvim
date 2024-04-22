ea1d2d7321b3d9cd6b59d7ef5e1f0dd411d9ae8c
Bergtrol <jannie.pieters@gmail.com>
2024-04-22 22:49:02 +0200

    Add tinygit plugin and config

---
+++ lua/custom/plugins/tinygit.lua
@@ -0,0 +1,26 @@
+-- tinygit
+-- https://github.com/windwp/nvim-autopairs
+
+return {
+  'chrisgrieser/nvim-tinygit',
+  ft = { 'git_rebase', 'gitcommit' }, -- so ftplugins are loaded
+  dependencies = {
+    'stevearc/dressing.nvim',
+    'nvim-telescope/telescope.nvim', -- either telescope or fzf-lua
+    'ibhagwan/fzf-lua',
+    'rcarriga/nvim-notify', -- optional, but will lack some features without it
+  },
+  opts = {
+    on_attach = function()
+      require('tinygit').smartCommit { pushIfClean = false } -- options default to `false`
+      require('nvim-tinygit').setup {}
+      vim.keymap.set('n', '<leader>hc', function()
+        require('tinygit').smartCommit()
+      end, { desc = 'Tinygit - SmartCommit' })
+      vim.keymap.set('n', '<leader>hp', function()
+        require('tinygit').push {}
+      end, { desc = 'Tinygit - Push' })
+    end,
+  },
+  config = function() end,
+}
